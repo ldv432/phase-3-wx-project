353dc7e457959106a169b790219a7a5ff9a1bc36
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-11-08 13:31:59 -0500

    updated cities&regions

--- lib/Cities.py
+++ lib/Cities.py
@@ -1,22 +1,41 @@
 class Cities:
+    all_cities = []
     def __init__(self ,id, name, region_id):
         self.id = id
         self.name = name
         self.region_id = region_id
+        Cities.all_cities.append(self)
         pass
 
-@property
-def id(self):
-    return self._id    
-@property
-def name(self):
-    return self._name    
-@property
-def region_id(self):
-    return self._region_id    
+    @property
+    def id(self):
+        return self._id    
+    @property
+    def name(self):
+        return self._name    
+    @property
+    def region_id(self):
+        return self._region_id    
 
-@id.setter
-def id(self,value):
-    self._id = value
-    if not isinstance(value,int):
-        raise ValueError("must be a integer")
+    @id.setter
+    def id(self,value):
+        if not isinstance(value,int):
+            raise ValueError("must be a integer")
+        self._id = value
+        # self._id = value (validation checks are always at the end)
+    
+    @name.setter
+    def name(self,value):
+        if not isinstance(value,str):
+            raise TypeError("must be of type string")
+        elif not 5<= len(value) <=50: 
+            #  elif not (5<= len(value) <=50):
+             raise ValueError("must be between 5 and 50 characters")
+        self._name = value
+    
+    @region_id.setter
+    def id(self,value):
+        if not isinstance(value,int):
+            raise ValueError("must be a integer")
+        self._region_id = value
+    
--- lib/Region.py
+++ lib/Region.py
@@ -1,4 +1,33 @@
 class Region:
-    def __init__(id, name):
+    all_regions = []
+    def __init__(self,id, name):
         self.id = id
-        pass
+        self.name = name
+        Region.all_regions.append(self)
+    
+
+    @property
+    def id(self):
+        return self._id  
+    @property
+    def name(self):
+        return self._name 
+      
+
+    @id.setter
+    def id(self,value):
+        if not isinstance(value,int):
+            raise ValueError("must be a integer")
+        self._id = value
+        # self._id = value (validation checks are always at the end)
+        
+    @name.setter
+    def name(self,value):
+        if not isinstance(value,str):
+            raise TypeError("must be of type string")
+        elif not 5<= len(value) <=50: 
+             raise ValueError("must be between 5 and 50 characters")  
+        elif hasattr(self, _name): 
+        self._name = value
+        # self._name = value (validation checks are always at the end)
+        
